7f5070f6b8b00e3b4cb23966b5b99e982bf5deac
Sougata Pal <palsougata256@gmail.com>
2025-03-23 16:42:17 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/",(req,res)=>{
     res.send("Subscribe to Sougata Pal's Channel")
 })
 
+app.get("/hello",(req,res)=>{
+    res.send("Hello Everyone!!")
+})
+
 app.listen(port,()=>{
     `Server started on port ${port}`
 })
